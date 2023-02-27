520415630006d80fe0064358abc490e5854412ca
mo0rain <962620891@qq.com>
2023-02-27 19:44:35 +0800

    xx

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,jsonify,redirect
 from markupsafe import escape
 
-app = Flask(__name__,instance_relative_config=True , template_folder="templates")
+app = Flask(__name__,instance_relative_config=True , template_folder="templates",static_folder="web\static")
 app.debug = True
 
 @app.route("/")
